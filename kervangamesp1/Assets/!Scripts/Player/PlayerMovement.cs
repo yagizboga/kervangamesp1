30684b8e9dc4b1446e72f6fb1f876444d0a2e304
Emir0zcelik <themirozcelik@gmail.com>
2024-07-27 13:35:09 +0300

    Player Movement with jump

--- kervangamesp1/Assets/!Scripts/Player/PlayerMovement.cs
+++ kervangamesp1/Assets/!Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float fallMultiplier;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody rb;
+    private bool isGrounded;
+    private Vector3 vecGravity;
+
+    private void Awake() {
+        vecGravity = new Vector3(0, -Physics2D.gravity.y, 0);
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        isGrounded = Physics.CheckCapsule(groundCheck.position, groundCheck.position + Vector3.down * 0.1f, 0.5f, groundLayer);
+        rb.velocity = new Vector3(movementSpeed * moveHorizontal, rb.velocity.y, 0);
+        
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, 0);        
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
+        }
+
         
     }
 }
