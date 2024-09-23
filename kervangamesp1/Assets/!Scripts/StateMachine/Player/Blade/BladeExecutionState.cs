79fd3919fb9a21694bda6c5ab962eda81cd966e4
Emir0zcelik <themirozcelik@gmail.com>
2024-09-23 23:21:27 +0300

    Reapply "Code ultimate, blade execution"
    This reverts commit cf64bddde8f285e8f5c2ac97b0f62ed817bbf1de.

--- kervangamesp1/Assets/!Scripts/StateMachine/Player/Blade/BladeExecutionState.cs
+++ kervangamesp1/Assets/!Scripts/StateMachine/Player/Blade/BladeExecutionState.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class BladeExecutionState : BladeState
 {
     public float executionTimer = 3f;
+
+    private KeyCode currentKey;
+    private List<KeyCode> keyList = new List<KeyCode> { KeyCode.H, KeyCode.J, KeyCode.K };
+    private bool isKeyCorrect = false;
     public BladeExecutionState(Blade blade) : base(blade)
     {
 
@@ -12,7 +16,7 @@ public class BladeExecutionState : BladeState
 
     public override void OnStateEnter()
     {
-
+        SetNewKey();
     }
 
     public override void OnStateExit()
@@ -28,12 +32,33 @@ public class BladeExecutionState : BladeState
     public override void OnStateUpdate()
     {
         CountDown();
+        CheckCorrectKeyPressed();
+    }
+
+    private void CheckCorrectKeyPressed()
+    {
+        if (isKeyCorrect) return;
+
+        if (Input.GetKeyDown(currentKey))
+        {
+            Debug.Log("Correct Key Pressed!");
+            isKeyCorrect = true;
+            SetNewKey();
+        }
+    }
+
+    private void SetNewKey()
+    {
+        currentKey = keyList[Random.Range(0, keyList.Count)];
+        isKeyCorrect = false;
+        Debug.Log("Execution skill's current key is: " + currentKey);
     }
 
     private void CountDown()
     {
         if (executionTimer <= 0)
         {
+            blade.bossCamera.Priority = 1;
             blade.ChangeState(new BladeMovingState(blade));
         }
 
